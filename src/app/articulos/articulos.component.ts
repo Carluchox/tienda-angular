044a2aa80325fdbd5c73d82669932b2bd47452a1
Carlos Reges <carluchox@mxlinux>
2022-10-08 22:24:24 -0300

    Mi primer proyecto de Angular propio

---
+++ src/app/articulos/articulos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-articulos',
+    templateUrl: './articulos.component.html',
+    styleUrls: ['./articulos.component.css']
+})
+
+export class ArticulosComponent{
+
+}
